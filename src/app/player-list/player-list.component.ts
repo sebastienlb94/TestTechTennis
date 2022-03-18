a5a0d3236af29aecf83f88c0592753c89f10afd2
sebastienlb94 <sebastiensotofr@gmail.com>
2022-03-18 22:11:39 +0100

    Data service + player list component

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Player } from '../types/player';
 
 @Component({
   selector: 'app-player-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-list.component.scss']
 })
 export class PlayerListComponent implements OnInit {
+  players: Player[] = [];
+  filteredString: string = '';
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getPlayers()
+      .subscribe((response:any) => {
+        this.players = response.players;
+      });
   }
 
 }

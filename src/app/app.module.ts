059b0b20704e39f2c79f8cd4698155c29ff966dd
sebastienlb94 <sebastiensotofr@gmail.com>
2022-03-18 20:12:40 +0100

    Architecture du projet

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlayerListComponent } from './player-list/player-list.component';
+import { PlayerDetailsComponent } from './player-details/player-details.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: "", component: PlayerListComponent},
+  { path: "player/:id", component: PlayerDetailsComponent},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayerListComponent,
+    PlayerDetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }

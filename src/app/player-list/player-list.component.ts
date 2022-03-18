ca06b84bc4b173295ba445fdd1cd59c8eb9a7afb
sebastienlb94 <sebastiensotofr@gmail.com>
2022-03-19 00:23:57 +0100

    Classement par rank

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -11,13 +11,18 @@ export class PlayerListComponent implements OnInit {
   players: Player[] = [];
   filteredString: string = '';
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService) {
+    this.dataService.getPlayers()
+      .subscribe((response: any) => {
+        this.players = response.players.sort((a: Player, b: Player) => {
+          if (a.data.rank < b.data.rank) return -1;
+          else if (a.data.rank > b.data.rank) return 1;
+          return 0;
+        });
+      });
+  }
 
   ngOnInit(): void {
-    this.dataService.getPlayers()
-      .subscribe((response:any) => {
-        this.players = response.players;
-      });
   }
 
 }
